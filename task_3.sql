9095bab57f20ee3f047bc42baefccab14eaac9c3
ericmuni-23 <munieric44@gmail.com>
2024-08-02 23:03:34 +0300

    A script that list all the tables

---
+++ task_3.sql
@@ -0,0 +1,5 @@
+-- Use the database specified in the command line argument
+-- Note: This script doesn't explicitly select a database since the database name will be passed via command line.
+
+-- List all tables in the database
+SHOW TABLES;
