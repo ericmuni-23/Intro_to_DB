8066c32d8492a9e2c87c253d17012fd9438aed31
ericmuni-23 <munieric44@gmail.com>
2024-08-26 03:22:35 +0300

    A script that lists all the tables.

--- task_3.sql
+++ task_3.sql
@@ -2,4 +2,4 @@
 -- Note: This script doesn't explicitly select a database since the database name will be passed via command line.
 
 -- List all tables in the database
-SHOW TABLES;
+SHOW TABLES; {'use alx_book_store;'}
