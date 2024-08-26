a815d543e04f40ba1d33d2124074a78074c61f2a
ericmuni-23 <munieric44@gmail.com>
2024-08-26 03:43:02 +0300

    A script that inserts a single row in the table.

--- task_5.sql
+++ task_5.sql
@@ -1,3 +1,5 @@
 -- Insert a single row into the Customers table
+USE $1;
 INSERT INTO CUSTOMER (customer_id, customer_name, email, address)
 VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
+-- Replace $1 with the actual database name
